a0cb24a7f253fcdaca506dc25d8846c4e3ad4045
ArenasITSE <EduardoArenas@escarcega.tecnm.mx>
2026-03-19 08:42:53 -0600

    eliminacion fichero anterios

--- DIRECCION180320260917/DIRECCION00/deploy.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Directorio Institucional", layout="wide")
-
-st.title("Directorio Institucional")
-st.write("Selecciona una página en el menú lateral.")
